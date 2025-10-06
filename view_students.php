027ed301bba5e26e6f0fd2c1bfde7a7cc9a18139
Sandamali19 <msandamali41@gmail.com>
2025-10-06 18:48:59 +0530

    update add style

--- view_students.php
+++ view_students.php
@@ -1,32 +1,38 @@
 <?php include('db_connect.php'); ?>
+<?php include('header.php'); ?>
+<link rel="stylesheet" href="style.css">
 
-<h2>Student List</h2>
-<a href="add_student.php">+ Add New Student</a><br><br>
+<div class="container">
+  <h2>Student List</h2>
+  <a href="add_student.php">
+    <button>+ Add New Student</button>
+  </a>
 
-<table border="1" cellpadding="8">
-  <tr>
-    <th>ID</th>
-    <th>Name</th>
-    <th>Grade</th>
-    <th>Email</th>
-    <th>Phone</th>
-    <th>Actions</th>
-  </tr>
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Grade</th>
+      <th>Email</th>
+      <th>Phone</th>
+      <th>Actions</th>
+    </tr>
 
-<?php
-$result = mysqli_query($conn, "SELECT * FROM students");
-while ($row = mysqli_fetch_assoc($result)) {
-  echo "<tr>
-          <td>{$row['id']}</td>
-          <td>{$row['name']}</td>
-          <td>{$row['grade']}</td>
-          <td>{$row['email']}</td>
-          <td>{$row['phone']}</td>
-          <td>
-            <a href='edit_student.php?id={$row['id']}'>Edit</a> |
-            <a href='delete_student.php?id={$row['id']}'>Delete</a>
-          </td>
-        </tr>";
-}
-?>
-</table>
+  <?php
+  $result = mysqli_query($conn, "SELECT * FROM students");
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>
+            <td>{$row['id']}</td>
+            <td>{$row['name']}</td>
+            <td>{$row['grade']}</td>
+            <td>{$row['email']}</td>
+            <td>{$row['phone']}</td>
+            <td class='actions'>
+              <a href='edit_student.php?id={$row['id']}'>✏️ Edit</a> |
+              <a href='delete_student.php?id={$row['id']}'>🗑️ Delete</a>
+            </td>
+          </tr>";
+  }
+  ?>
+  </table>
+</div>
