c0ace941ca33b197ab2aa664e7b6c6a935f683ad
Sandamali19 <msandamali41@gmail.com>
2025-10-06 18:49:51 +0530

    add style

--- edit_student.php
+++ edit_student.php
@@ -1,26 +1,31 @@
 <?php
 include('db_connect.php');
+include('header.php');
 $id = $_GET['id'];
 $result = mysqli_query($conn, "SELECT * FROM students WHERE id=$id");
 $row = mysqli_fetch_assoc($result);
 ?>
+<link rel="stylesheet" href="style.css">
 
-<h2>Edit Student</h2>
-<form method="POST" action="">
-  <label>Name:</label><br>
-  <input type="text" name="name" value="<?php echo $row['name']; ?>" required><br><br>
+<div class="container">
+  <h2>Edit Student</h2>
+  <form method="POST" action="">
+    <label>Name:</label>
+    <input type="text" name="name" value="<?php echo $row['name']; ?>" required>
 
-  <label>Grade:</label><br>
-  <input type="text" name="grade" value="<?php echo $row['grade']; ?>" required><br><br>
+    <label>Grade:</label>
+    <input type="text" name="grade" value="<?php echo $row['grade']; ?>" required>
 
-  <label>Email:</label><br>
-  <input type="email" name="email" value="<?php echo $row['email']; ?>"><br><br>
+    <label>Email:</label>
+    <input type="email" name="email" value="<?php echo $row['email']; ?>">
 
-  <label>Phone:</label><br>
-  <input type="text" name="phone" value="<?php echo $row['phone']; ?>"><br><br>
+    <label>Phone:</label>
+    <input type="text" name="phone" value="<?php echo $row['phone']; ?>">
 
-  <input type="submit" name="update" value="Update">
-</form>
+    <input type="submit" name="update" value="Update Student">
+  </form>
+  <br>
+  <a href="view_students.php">← Back to Student List</a>
 
 <?php
 if (isset($_POST['update'])) {
@@ -29,14 +34,13 @@ if (isset($_POST['update'])) {
   $email = $_POST['email'];
   $phone = $_POST['phone'];
 
-  $sql = "UPDATE students 
-          SET name='$name', grade='$grade', email='$email', phone='$phone' 
-          WHERE id=$id";
+  $sql = "UPDATE students SET name='$name', grade='$grade', email='$email', phone='$phone' WHERE id=$id";
 
   if (mysqli_query($conn, $sql)) {
-    echo "<p>Student updated successfully!</p>";
+    echo "<p style='color:green;'>✔ Student updated successfully!</p>";
   } else {
-    echo "Error updating: " . mysqli_error($conn);
+    echo "<p style='color:red;'>Error updating: " . mysqli_error($conn) . "</p>";
   }
 }
 ?>
+</div>
